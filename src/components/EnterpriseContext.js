d1d82b104ba741616ee28041ce7517e7f5c7454a
Alberto de Sá Cavalcanti de Albuquerque <albertoalbuquerque01@gmail.com>
2020-01-24 14:26:27 -0300

    Add Warp Drive behaviour

--- src/components/EnterpriseContext.js
+++ src/components/EnterpriseContext.js
@@ -4,9 +4,27 @@ const EnterpriseContext = createContext();
 
 const EnterpriseProvider = ({ children }) => {
   const [energyGauge, setEnergyGauge] = useState(0);
+  const [warpSpeed, setWarpSpeed] = useState(false);
+
+  const engageWarpSpeed = () => {
+    if (weaponsEnabled || shieldsOnline || energyGauge > 2) return undefined;
+
+    setWarpSpeed(true);
+    setEnergyGauge(2);
+  };
+
+  const disengageWarpSpeed = () => {
+    if (!warpSpeed) return undefined;
+
+    setWarpSpeed(false);
+    setEnergyGauge(energyGauge - 2);
+  };
 
   const context = {
     energyGauge,
+    warpSpeed,
+    engageWarpSpeed,
+    disengageWarpSpeed,
   };
 
   return (
--- src/components/WarpDrive.js
+++ src/components/WarpDrive.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { EnterpriseContext } from './EnterpriseContext.js';
 
 import './Panel.css';
 
 export default function WarpDrive() {
+  const { warpSpeed, engageWarpSpeed, disengageWarpSpeed } = useContext(
+    EnterpriseContext,
+  );
+
   return (
     <div className="panel">
       <span>Motor de Dobra</span>
-      <div className={`alert-light`} />
-        <button onClick={() => alert('clicou')}>
-        {`PLACEHOLDER`} velocidade de dobra
+      <div className={`alert-light ${warpSpeed ? 'green-light' : 'red-light'}`} />
+      <button onClick={warpSpeed ? disengageWarpSpeed : engageWarpSpeed}>
+        {`${warpSpeed ? 'Desengajar' : 'Engajar'}`} velocidade de dobra
       </button>
       <span>Armas e escudos devem estar desligados</span>
     </div>
